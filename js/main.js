b344593124d37db27f66948c52903d142d4c1ee8
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-12-09 21:34:11 +0200

    Learn about operator and build baby weather app

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-/////////////////////////////////////////////////////////
-// Learn Basic about console.log, variables, comment and operators.
-////////////////////////////////////////////////////////
 console.log("hello world");
 // Do not forget quote around the sting
 console.log("Ahmed Eid");
 
 // Variables sores something to use
-name = "Ahmed";
+// var => do not use
+// const => use it if the data does not change
+// let => this common one to use
+let name = "Ahmed";
 console.log(name);
 
-sentence =
+let sentence =
   "How are you doing today, nice to see you, hope you have a great day!";
 console.log(sentence);
 
@@ -29,17 +29,55 @@ console.log(sentence);
 // user input
 
 // * / ** + - %
-food = prompt("how much was the food? ");
-tipPercantage = prompt("tip %? ") / 100;
-tipAmount = food * tipPercantage;
 
-total = Number(food) + Number(tipAmount);
+// food = prompt('how much was the food? ')
+// tipPercantage = prompt('tip %? ') / 100
+// tipAmount = food * tipPercantage
 
-console.log("Tip Amount: " + tipAmount);
-// alert(tipAmount)
-console.log("Total: " + total);
+// total = Number(food) + Number(tipAmount);
 
-// Data types [String, numbers]
+// console.log('Tip Amount: ' + tipAmount)
+// // alert(tipAmount)
+// console.log('Total: ' + total)
+
+/* Data types [String, numbers]
+String => 'hello', "Ahmed"
+Number => 1, 23, 5, 6, 7.5
+Array => []
+Object => {}
+Boolean => true / false
+*/
+
+/* Math Operator
+Mutiply *
+Divide /
+Exponents **
+Remainder %
+Add +
+Subtract -
+*/
+
+/* Math Methods
+Floor => Rounds down
+Ceil => Rounds up
+Rondom => Give random number between 0 and 1
+*/
+
+////////////////////////////////
+// Baby weather app (conditionals)
+///////////////////////////////
+// if rain 'Grab your umberlla ☔'
+// else 'Wear your sun glasses'
+let weather = prompt("What is the weather today? ");
+
+if (weather === "rainy") {
+  console.log("Grab your umbrella ☔");
+} else {
+  console.log("Wear your Glasses 🕶️");
+}
+
+// Conditional Operator
+// ===, ==, <, >, <=, >=, !==, !=
 
 // Function
 

046b2b966212e393f9b6615f389a8eb773e202d4
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-12-09 20:11:25 +0200

    Learn basic about console.log, variables, comment and operators

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////
+// Learn Basic about console.log, variables, comment and operators.
+////////////////////////////////////////////////////////
+console.log("hello world");
+// Do not forget quote around the sting
+console.log("Ahmed Eid");
+
+// Variables sores something to use
+name = "Ahmed";
+console.log(name);
+
+sentence =
+  "How are you doing today, nice to see you, hope you have a great day!";
+console.log(sentence);
+
+// Operator
+// food = 20
+// console.log(food)
+// always give descriptive meaning of your variables
+// tipPercantage = 0.2
+// tipAmount = food * tipPercantage
+
+// console.log(tipAmount)
+
+// Prompt => This js built in function that Get input from users
+// fruit = prompt('What is your favorite fruit? ')
+// console.log(fruit)
+
+// user input
+
+// * / ** + - %
+food = prompt("how much was the food? ");
+tipPercantage = prompt("tip %? ") / 100;
+tipAmount = food * tipPercantage;
+
+total = Number(food) + Number(tipAmount);
+
+console.log("Tip Amount: " + tipAmount);
+// alert(tipAmount)
+console.log("Total: " + total);
+
+// Data types [String, numbers]
+
+// Function
+
+// Type Casting

c7713195638b48edd99b07d0db939cc3efeb4b99
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-12-16 22:47:07 +0200

    DOM

--- js-assignments/assignments2.js
+++ js-assignments/assignments2.js
@@ -167,3 +167,239 @@
 //   });
 
 // console.log(res.slice(+false, res.length - +true));
+
+//////////////////////////////////////////
+// OBJECT AND METHODS
+/////////////////////////////////////////
+// #1
+// Create Your Object Here
+// let member = {
+//   name: "Elzero",
+//   age: 38,
+//   country: "Egypt",
+//   fullDetails: function () {
+//     return `My Name Is ${this.name}, My Age Is ${this.age}, I Live in ${this.country}`;
+//   },
+// };
+
+// console.log(member.name); // Elzero
+// console.log(member.age); // 38
+// console.log(member.country); // Egypt
+// console.log(member.fullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
+
+// #2
+// Method One
+// let objMethodOne = { property: "Method One" };
+// console.log(objMethodOne.property); // "Method One"
+
+// Method Two
+// let objMethodTwo = {};
+// objMethodTwo.property = "Method Two";
+// console.log(objMethodTwo.property); // "Method Two"
+
+// Method Three
+// let objMethodThree = Object.create({ property: "Method Three" });
+// console.log(objMethodThree.property); // "Method Three"
+
+// Method Four
+// let objMethodFour = new Object({ property: "Method Four" });
+// console.log(objMethodFour.property); // "Method Four"
+
+// #3
+// let a = 1;
+
+// let threeNums = {
+//   b: 2,
+//   c: 3,
+//   d: 4,
+// };
+
+// let twoNums = {
+//   e: 5,
+//   f: 6,
+// };
+
+// Create Your Object Here in One Line
+// let finalObject = Object.assign({ a }, threeNums, twoNums);
+// console.log(finalObject);
+
+// #4
+// The Object To Work With
+// let myFavGames = {
+//   "Trinity Universe": {
+//     publisher: "NIS America",
+//     price: 40,
+//   },
+//   "Titan Quest": {
+//     publisher: "THQ",
+//     bestThree: {
+//       one: "Immortal Throne",
+//       two: "Ragnarök",
+//       three: "Atlantis",
+//     },
+//     price: 50,
+//   },
+//   YS: {
+//     publisher: "Falcom",
+//     bestThree: {
+//       one: "Oath in Felghana",
+//       two: "Ark Of Napishtim",
+//       three: "origin",
+//     },
+//     price: 40,
+//   },
+// };
+
+// // Code One => How To Get Object Length ?
+// let objectLength = Object.keys(myFavGames).length; // 3
+// let gameName = Object.keys(myFavGames);
+
+// for (let i = 0; i < objectLength; i++) {
+//   console.log(`The Game Name Is ${gameName[i]}`);
+//   console.log(`The Publisher Is ${myFavGames[gameName[i]].publisher}`);
+//   console.log(`The Price Is ${myFavGames[gameName[i]].price}`);
+
+//   // Check If Nested Object Has Property (bestThree)
+//   if (myFavGames[gameName[i]].hasOwnProperty("bestThree")) {
+//     console.log("- Game Has Releases");
+//     console.log(`First => ${myFavGames[gameName[i]].bestThree.one}`);
+//     console.log(`Second => ${myFavGames[gameName[i]].bestThree.two}`);
+//     console.log(`Third => ${myFavGames[gameName[i]].bestThree.three}`);
+//   }
+//   console.log("#".repeat(20));
+// }
+
+// // Ouput
+
+// ("The Game Name Is Trinity Universe");
+// ("The Publisher Is NIS America");
+// ("The Price Is 40");
+// ("####################");
+// ("The Game Name Is Titan Quest");
+// ("The Publisher Is THQ");
+// ("The Price Is 50");
+// ("- Game Has Releases");
+// ("First => Immortal Throne");
+// ("Second => Ragnarök");
+// ("Third => Atlantis");
+// ("####################");
+// ("The Game Name Is YS");
+// ("The Publisher Is Falcom");
+// ("The Price Is 40");
+// ("- Game Has Releases");
+// ("First => Oath in Felghana");
+// ("Second => Ark Of Napishtim");
+// ("Third => origin");
+// ("####################");
+
+//////////////////////////////////////////
+// DOCUMENT OBJECT MODEL
+/////////////////////////////////////////
+// #1
+{
+  /* <div id="elzero" class="element" name="js">JavaScript</div>; */
+}
+
+// console.log(document.getElementById("elzero"));
+// console.log(document.getElementsByClassName("element")[0]);
+// console.log(document.getElementsByTagName("div")[0]);
+// console.log(document.querySelector("#elzero"));
+// console.log(document.querySelector(".element"));
+// console.log(document.querySelector("div"));
+// console.log(document.querySelector("[name='js']"));
+// console.log(document.querySelectorAll("#elzero")[0]);
+// console.log(document.querySelectorAll(".element")[0]);
+// console.log(document.querySelectorAll("div")[0]);
+// console.log(document.querySelectorAll("[name='js']")[0]);
+// console.log(document.body.children[0]);
+// console.log(document.body.childNodes[7]);
+// console.log(document.body.innerHTML);
+
+// #2
+// let imgs = document.images;
+// console.log(imgs);
+// document.body.style.background = "black";
+
+// imgs.map((img) => {
+//   img.src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+//   img.alt = "Elzero Logo";
+// });
+
+// for (let img of imgs) {
+//   img.setAttribute(
+//     "src",
+//     "https://elzero.org/wp-content/themes/elzero/imgs/logo.png"
+//   );
+//   // img.src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+//   img.alt = "Elzero Logo";
+// }
+
+// #3
+// let input = document.querySelector("[name='dollar'");
+// let text = document.querySelector(".result");
+// console.log(text);
+
+// input.onkeyup = () => {
+//   input.value ? input.value : (input.value = 0);
+//   text.textContent = `${parseFloat(input.value)} USD Dollar => ${(
+//     parseFloat(input.value) * 15.6
+//   ).toFixed(2)} EGP Pound`;
+// };
+
+// #4
+// let one = document.querySelector(".one");
+// let two = document.querySelector(".two");
+// let text = one.textContent;
+
+// one.title = one.className;
+// two.title = two.className;
+
+// one.textContent = two.textContent;
+// two.textContent = text + ` ${document.querySelectorAll("div").length}`;
+
+// console.log(one);
+// console.log(two);
+
+// #5
+// document.images.forEach((img) => {
+//   if (img.hasAttribute("alt")) {
+//     img.setAttribute("alt", "old");
+//   } else img.setAttribute("alt", "Elzero New");
+// });
+// let images = document.images;
+
+// for (let img of images) {
+//   if (img.hasAttribute("alt")) {
+//     img.setAttribute("alt", "old");
+//   } else img.setAttribute("alt", "Elzero New");
+// }
+
+// #6
+// let numElements = document.querySelector('[name="elements"]');
+// let text = document.querySelector('[name="texts"]');
+// let element = document.querySelector("[name='type']");
+// let results = document.querySelector(".results");
+// let create = document.querySelector('[name="create"');
+
+// create.onclick = (e) => {
+//   e.preventDefault();
+
+//   if (results.children.length > 0) {
+//     results.innerHTML = "";
+//   }
+
+//   for (let i = 1; i <= numElements.value; i++) {
+//     let createElement = document.createElement(`${element.value}`);
+
+//     createElement.textContent = text.value;
+
+//     createElement.className = "box";
+
+//     createElement.setAttribute("id", `id-${i}`);
+
+//     createElement.setAttribute("title", "Element");
+
+//     results.appendChild(createElement);
+//   }
+// };

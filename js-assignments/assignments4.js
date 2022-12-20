11f141d00cad38911ac307dfb49914f77c1d5fa8
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-12-20 21:57:31 +0200

    BOM

---
+++ js-assignments/assignments4.js
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////
+// BOM CHALLENGES
+///////////////////////////////////////////////////
+// #1
+// let number = prompt("Print Number From-To")
+//   .split("-")
+//   .sort((a, b) => a - b);
+
+// console.log(number);
+
+// for (let i = +number[0]; i <= +number[1]; i++) {
+//   console.log(i);
+// }
+
+// #2
+
+// let popup = document.querySelector(".popup");
+// let close = document.querySelector("button");
+
+// let popUp = () => {
+//   popup.style.display = "block";
+// };
+
+// setTimeout(popUp, 5000);
+
+// close.onclick = () => (popup.style.display = "none");
+
+// #3
+// let number = document.querySelector(".counter");
+
+// let counter = () => {
+//   if (number.innerHTML > 0) {
+//     number.innerHTML -= 1;
+//   } else {
+//     clearInterval(counter2);
+//   }
+// };
+
+// let counter2 = setInterval(counter, 1000);
+
+// #4
+// let number = document.querySelector(".counter");
+
+// let counter = () => {
+//   if (number.innerHTML > 0) {
+//     number.innerHTML -= 1;
+//   } else {
+//     window.open(
+//       "https://elzero.org/category/assignments/javascript-bootcamp-assignments/"
+//     );
+//   }
+// };
+
+// let counter2 = setInterval(counter, 1000);
+
+// #5
+// let number = document.querySelector(".counter");
+
+// let counter = () => {
+//   if (number.innerHTML > 0) {
+//     number.innerHTML -= 1;
+//     if (number.innerHTML == "5") {
+//       window.open(
+//         "https://elzero.org/category/assignments/javascript-bootcamp-assignments/",
+//         "_blank",
+//         "width=500,height=500"
+//       );
+//     }
+//   } else {
+//     clearInterval(counter2);
+//   }
+// };
+
+// let counter2 = setInterval(counter, 1000);
